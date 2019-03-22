48cf41ec4353144f1aafb4c382218fcad0d7852d
Alexis <alexisjmp@gmail.com>
2019-03-22 12:39:25 -0300

    comenta cls_atencion

--- negocio/datos.php
+++ negocio/datos.php
@@ -1,8 +1,9 @@
 <?php
 require_once('../clases/cls_paciente.php');
-require_once('../clases/cls_atencion.php');
+//require_once('../clases/cls_atencion.php');
 require_once('funciones_adicionales.php');
 //echo "datos.php"
+
 
 if($_POST['funcion']== 'minInsertPaciente')
      minInsertPaciente();
@@ -13,14 +14,14 @@ if($_POST['funcion']== 'cargaTabla')
 function minInsertPaciente(){
    // echo "Paciente registrado..";
     
-    unset($nodo);
-    $nodo = new paciente();
+   // unset($nodo);
+ $nodo = new paciente();
 ////    $fecha_nacimiento = $_POST['fecha_nacimiento'];
-    $nodo->insertar($_POST['rut'],$_POST['verificador'],$_POST['nombre'],$_POST['paterno'],$_POST['materno'],$_POST['fecha_nacimiento'],
-            $_POST['fijo'],$_POST['movil'],$_POST['nacionalidad'],$_POST['profesion']);
-
-    $resp = $nodo->buscarId($_POST['rut']);
-    echo $resp;
+ $nodo->insertar($_POST['rut'],$_POST['verificador'],$_POST['nombre'],$_POST['paterno'],$_POST['materno'],$_POST['fecha_nacimiento'],
+       $_POST['fijo'],$_POST['movil'],$_POST['nacionalidad'],$_POST['profesion']);
+ echo  $nodo->getid_paciente();
+   // $resp = $nodo->buscarId($_POST['rut']);
+ 
 
 }
 
